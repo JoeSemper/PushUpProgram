bfe7fe469b6c426cc3af867f4d980c0dbaee6a8b
JoeSemper <joe.semper@gmail.com>
2023-03-29 16:25:35 +0300

    Bottom navigation impl

--- app/src/main/java/com/joesemper/pushupprogram/AppNavigation.kt
+++ app/src/main/java/com/joesemper/pushupprogram/AppNavigation.kt
@@ -1,18 +1,55 @@
 package com.joesemper.pushupprogram
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.joesemper.pushupprogram.ui.screens.home.HomeScreen
+import com.joesemper.pushupprogram.ui.screens.progress.ProgressScreen
+import com.joesemper.pushupprogram.ui.screens.settings.SettingsScreen
 import com.joesemper.pushupprogram.ui.screens.workout.WorkoutScreen
 
 const val HOME_ROUTE = "home"
+const val PROGRESS_ROUTE = "Progress"
+const val SETTINGS_ROUTE = "settings"
 const val WORKOUT_ROUTE = "workout/{workoutId}"
+
+sealed class Screen(val route: String, @StringRes val labelRes: Int, val icon: ImageVector) {
+    object Home : Screen(
+        route = HOME_ROUTE,
+        labelRes = R.string.home,
+        icon = Icons.Default.Home
+    )
+
+    object Progress : Screen(
+        route = PROGRESS_ROUTE,
+        labelRes = R.string.progress,
+        icon = Icons.Default.AccountCircle
+    )
+
+    object Settings : Screen(
+        route = SETTINGS_ROUTE,
+        labelRes = R.string.settings,
+        icon = Icons.Default.Settings
+    )
+}
 
 @Composable
 fun AppNavHost(
@@ -20,23 +57,66 @@ fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     startDestination: String = HOME_ROUTE
 ) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = startDestination
-    ) {
-        composable(
-            route = HOME_ROUTE
-        ) {
-            HomeScreen(navController)
-        }
+    val bottomNavItems = listOf(
+        Screen.Home,
+        Screen.Progress,
+        Screen.Settings
+    )
 
-        composable(
-            route = WORKOUT_ROUTE,
-            arguments = listOf(navArgument("workoutId") { type = NavType.StringType})
-        ) {
-            WorkoutScreen(navController)
-        }
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
 
+                bottomNavItems.forEach { screen ->
+                    BottomNavigationItem(
+                        icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                        label = { Text(text = stringResource(id = screen.labelRes)) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        })
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            modifier = modifier.padding(innerPadding),
+            navController = navController,
+            startDestination = startDestination
+        ) {
+            composable(
+                route = Screen.Home.route
+            ) {
+                HomeScreen(navController)
+            }
+
+            composable(
+                route = Screen.Progress.route
+            ) {
+                ProgressScreen(navController)
+            }
+
+            composable(
+                route = Screen.Settings.route
+            ) {
+                SettingsScreen(navController)
+            }
+
+            composable(
+                route = WORKOUT_ROUTE,
+                arguments = listOf(navArgument("workoutId") { type = NavType.StringType })
+            ) {
+                WorkoutScreen(navController)
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/joesemper/pushupprogram/ui/screens/progress/ProgressScreen.kt
@@ -0,0 +1,10 @@
+package com.joesemper.pushupprogram.ui.screens.progress
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ProgressScreen(navController: NavController) {
+    Text(text = "Progress")
+}
